329c119a9c0d61b5e7b3abda72d4dac4a7067fa5
Amoralch <106614528+Amoralch@users.noreply.github.com>
2024-11-27 12:58:11 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"isuct.ru/informatics2022/lab4"
 	"isuct.ru/informatics2022/lab6"
+	"isuct.ru/informatics2022/lab7"
 )
 
 func main() {
 	lab4.RunLab4Tasks()
 	lab6.RunLab6()
+	lab7.RunLab7()
 }
