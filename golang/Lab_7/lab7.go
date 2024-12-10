e80dc1207a0d9e9ca1040a28879570335200722f
Amoralch <106614528+Amoralch@users.noreply.github.com>
2024-12-10 20:10:51 +0300

    Update lab7.go

--- golang/Lab_7/lab7.go
+++ golang/Lab_7/lab7.go
@@ -6,9 +6,9 @@ import (
 
 type Product interface {
 	GetPrice() float64
-	SetPrice(price float64)
-	ApplyDiscount(discount float64) 
-	GetDetails() string          
+	SetPrice(float64)
+	ApplyDiscount(float64)
+	GetDetails() string
 }
 
 type BaseProduct struct {
@@ -25,11 +25,11 @@ func (p *BaseProduct) SetPrice(price float64) {
 }
 
 func (p *BaseProduct) ApplyDiscount(discount float64) {
-	if discount >= 0 && discount <= 100 {
-		p.Price = p.Price * (1 - discount/100)
-	} else {
-		fmt.Println("Скидка должна быть в диапазоне от 0 до 100%.")
+	if discount < 0 || discount > 100 {
+		fmt.Println("Скидка должна быть от 0 до 100%")
+		return
 	}
+	p.Price *= (1 - discount/100)
 }
 
 func (p *BaseProduct) GetDetails() string {
@@ -38,27 +38,27 @@ func (p *BaseProduct) GetDetails() string {
 
 type Electronics struct {
 	BaseProduct
-	Warranty int 
+	Warranty int
 }
 
 func (e *Electronics) GetDetails() string {
-	return fmt.Sprintf("%s, Гарантия: %d лет", e.BaseProduct.GetDetails(), e.Warranty)
+	return fmt.Sprintf("%s, Гарантия: %d года", e.BaseProduct.GetDetails(), e.Warranty)
 }
 
 type Clothing struct {
 	BaseProduct
-	Size  string 
-	Color string 
+	Size  string
+	Color string
 }
 
 func (c *Clothing) GetDetails() string {
 	return fmt.Sprintf("%s, Размер: %s, Цвет: %s", c.BaseProduct.GetDetails(), c.Size, c.Color)
 }
 
-func CalculateTotalPrice(products []Product) float64 {
-	total := 0.0
-	for _, product := range products {
-		total += product.GetPrice()
+func TotalPrice(products []Product) float64 {
+	var total float64
+	for _, p := range products {
+		total += p.GetPrice()
 	}
 	return total
 }
@@ -77,22 +77,19 @@ func RunLab7() {
 	products := []Product{laptop, tshirt}
 
 	fmt.Println("Список товаров:")
-	for _, product := range products {
-		fmt.Println(product.GetDetails())
+	for _, p := range products {
+		fmt.Println(p.GetDetails())
 	}
 
-	totalPrice := CalculateTotalPrice(products)
-	fmt.Printf("\nОбщая стоимость до скидок: %.2f\n", totalPrice)
+	fmt.Printf("\nОбщая стоимость до скидок: %.2f\n", TotalPrice(products))
 
 	laptop.ApplyDiscount(10)
-	tshirt.ApplyDiscount(20) 
+	tshirt.ApplyDiscount(20)
 
-	fmt.Println("\nСписок товаров после применения скидок:")
-	for _, product := range products {
-		fmt.Println(product.GetDetails())
+	fmt.Println("\nСписок товаров после скидок:")
+	for _, p := range products {
+		fmt.Println(p.GetDetails())
 	}
 
-	totalPriceAfterDiscount := CalculateTotalPrice(products)
-	fmt.Printf("\nОбщая стоимость после скидок: %.2f\n", totalPriceAfterDiscount)
+	fmt.Printf("\nОбщая стоимость после скидок: %.2f\n", TotalPrice(products))
 }
-
