a5277fc3db3ba0b576a6bc460a00fd71ec264b2e
Amoralch <106614528+Amoralch@users.noreply.github.com>
2024-12-11 17:11:52 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"isuct.ru/informatics2022/lab4"
 )
 
 func main() {
-Lab_4
 	lab4.RunLab4Tasks()
-
 	fmt.Println("Огудин Вячеслав Михайлович")
-Ogudin_Vyacheslav
 }
